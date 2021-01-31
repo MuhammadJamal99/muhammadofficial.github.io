e6e32f0beb0664ca37e7b479d2473ebd72e2e477
Muhammad Jamal <MuhammadJamal99@users.noreply.github.com>
2021-01-31 07:17:12 +0200

    last update

--- phone_shop/public/assets/js/app.js
+++ phone_shop/public/assets/js/app.js
@@ -1,8 +1,8 @@
 $(function(){
   'use strict';
-
+  /*fix fixed top*/ 
   $("body").css("padding-top", $(".navbar.fixed-top").height());
-
+  /* pop up*/ 
   const popup = $('.popup');
   const closePopup = $('.popup-close');
   if(popup){
@@ -15,14 +15,30 @@ $(function(){
       }, 500);
     });
   }
-  /*side bar*/ 
+  /* sidebar */ 
   $('#sidebarCollapse').on('click', function () {
 		$('#sidebar-menu').toggleClass('active');
 	});
 	
 	$('#close-sidebar').on('click', function () {
 		$('#sidebar-menu').toggleClass('active');
-	});
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
+  // scroll body to 0px on click
+  $('#back-to-top').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 400);
+    return false;
+  });
+
 });
 
 // initialize AOS Animation
